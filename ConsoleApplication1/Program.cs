9bc5488dc2c15aa55af4fd145b343e2f0e95bc5f
RayLamOffice <RayLamOffice@RAYLAMOFFICE-PC>
2017-05-23 13:25:29 -0400

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void writeLog()
+        {
+            string logUrl = "./log/Log.txt";
+            if (!File.Exists(logUrl))
+            {
+                System.Console.WriteLine("Log doesn't exists, creating...");
+                File.Create(logUrl);
+            }
+
+
+        }
+        static void readConf()
+        {
+            System.Console.WriteLine("Hello world!");
+        }
+
+        static void Main(string[] args)
+        {
+            writeLog();
+        }
+    }
+}
