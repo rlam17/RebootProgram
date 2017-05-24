95b5c49ff9ca57ad5f27ae587518a58be7188e85
Alex Kong <Alex Kong@ALEXKONG-PC>
2017-05-24 11:41:56 -0400

    Experimental addition for robutsness

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -345,17 +345,22 @@ namespace Websdepot
                     string[] strRegEx = new string[] { ".exe " };
                     string[] strSplit;
                     strArgs = " ";
-                    strSplit = strChunk.Split(strRegEx, 2, StringSplitOptions.None);
-                    //strSplit[0] = strSplit[0] + ".exe";
-                    strPath = strSplit[0];
                     try
                     {
-                        strArgs = strSplit[1];
-                    } catch (Exception e)
+                        strSplit = strChunk.Split(strRegEx, 2, StringSplitOptions.None);
+                        strPath = strSplit[0];
+                        try
+                        {
+                            strArgs = strSplit[1];
+                        }
+                        catch (Exception e) { }
+                    }
+                    catch (Exception e)
                     {
-
+                        strPath = strChunk;
                     }
                     
+        
 
                     System.Console.WriteLine(strPath + " | " + strArgs);
                     Process.Start(strPath, strArgs);
